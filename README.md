b28fb8e8e8af4ee70545518a32ba6a1457542050
Nitghwolf <nitghwolf13@gmail.com>
2024-10-27 19:42:38 +0300

    add config

---
+++ README.md
@@ -0,0 +1,1 @@
+This is full React prod project
---
+++ config/build/types/config.ts
@@ -0,0 +1,19 @@
+export type BuildModeType = "development" | "production";
+
+export interface BuildPaths {
+  entry: string;
+  build: string;
+  html: string;
+}
+
+export interface BuildEnv {
+  mode: BuildModeType;
+  port: number;
+}
+
+export interface BuildOptions {
+  mode: BuildModeType;
+  paths: BuildPaths;
+  isDev: boolean;
+  port: number;
+}
