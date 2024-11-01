3efeb29f3d4a54bda17a76033246e8bcdb9b422e
Nitghwolf <nitghwolf13@gmail.com>
2024-11-01 18:20:32 +0300

    refactoring routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,38 +1,19 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { Route, Routes } from "react-router-dom";
-import "./styles/index.scss";
 import { useTheme } from "app/providers/ThemeProvider";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
 import { classNames } from "shared/lib/classNames/classNames";
+import { AppRouter } from "./providers/router";
+import "./styles/index.scss";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={classNames('app', {}, [theme])}>
+    <div className={classNames("app", {}, [theme])}>
       <Link to={"/"}>Main</Link>
       <Link to={"/about"}>About</Link>
       <button onClick={toggleTheme}>TOGGLE THEME</button>
-      <Routes>
-        <Route
-          path={"/about"}
-          element={
-            <Suspense fallback={"Loading"}>
-              <AboutPage />
-            </Suspense>
-          }
-        />
-        <Route
-          path={"/"}
-          element={
-            <Suspense fallback={"Loading"}>
-              <MainPage />
-            </Suspense>
-          }
-        />
-      </Routes>
+      <AppRouter />
     </div>
   );
 };
