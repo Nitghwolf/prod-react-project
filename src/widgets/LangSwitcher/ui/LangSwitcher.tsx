2172296e3c4b377fdb4fbbf02edbfed09037060d
Nitghwolf <nitghwolf13@gmail.com>
2025-02-24 00:10:06 +0300

    add build analyzer

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { classNames } from "shared/lib/classNames/classNames";
-import Button, { ThemeButton } from "shared/ui/AppLink/Button/Button";
 import { useTranslation } from "react-i18next";
+import { classNames } from "shared/lib/classNames/classNames";
+import  Button, { ThemeButton } from "shared/ui/AppLink/Button/Button";
 
 interface LangSwitcherProps {
   className?: string;
