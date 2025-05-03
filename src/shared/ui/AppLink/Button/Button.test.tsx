49c730a5c2218cda94fd5124fc0ed7b40f708d4c
Wolfy <nitghwolf13@gmail.com>
2025-05-03 18:12:18 +0300

    add test cases

--- src/shared/ui/AppLink/Button/Button.test.tsx
+++ src/shared/ui/AppLink/Button/Button.test.tsx
@@ -5,13 +5,11 @@ import Button, {
 
 describe('ButtonTest', () => {
   test('Test render', () => {
-    // eslint-disable-next-line i18next/no-literal-string
     render(<Button>Test</Button>);
     expect(screen.getByText('Test')).toBeInTheDocument();
   });
 
   test('Test clear render', () => {
-    // eslint-disable-next-line i18next/no-literal-string
     render(<Button theme={ThemeButton.CLEAR}>Test</Button>);
     expect(screen.getByText('Test')).toHaveClass('clear');
     screen.debug();
